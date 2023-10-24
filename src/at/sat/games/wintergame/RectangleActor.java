1297becf2b5e8e9dea5b89eeaaea0a78a405b87d
serkan <serkan.satirx61@gmail.com>
2023-10-24 12:18:48 +0200

    playing around with graphics

---
+++ src/at/sat/games/wintergame/RectangleActor.java
@@ -0,0 +1,26 @@
+package at.sat.games.wintergame;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+
+public class RectangleActor {
+    private float x, y;
+
+    public RectangleActor(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public void update(GameContainer gc, int delta) {
+        this.x += 0.2;
+        if (this.x > 810) {
+            this.x = 0;
+        }
+    }
+
+    public void render(Graphics graphics) {
+        graphics.drawRect(this.x, this.y, 40, 20);
+        graphics.fillRect(this.x, this.y, 40, 20);
+    }
+}
