96f5b7f5ebd87c437d14a19c9fc3b56c70ba5374
serkan <serkan.satirx61@gmail.com>
2023-11-08 11:08:02 +0100

    Started Strategy Pattern added MoveRight and implemendet it on CircleActor

---
+++ src/at/sat/games/wintergame/MoveRight.java
@@ -0,0 +1,32 @@
+package at.sat.games.wintergame;
+
+public class MoveRight {
+    private float x,y, speed;
+
+    public MoveRight(float x, float y, float speed) {
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+
+    
+    public void update(int delta) {
+        this.x += delta * speed;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+}
